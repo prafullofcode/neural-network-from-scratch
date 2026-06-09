cfe68c3b1fe9aec4a6dc097f1bbf7834d96c8653
prafullofcode <dahal.praphul268@gmail.com>
2026-06-09 21:51:05 +0545

    refactor: rename nnfs to my_nnfs to fix import conflicts

---
+++ my_nnfs/__init__.py
@@ -0,0 +1,4 @@
+from .layers import Layer_Dense
+from .activations import Activation_ReLU , Activation_Softmax
+from .losses import Activation_Softmax_Loss_CategoricalCrossentropy
+from .optimizers import Optimizer_SGD
