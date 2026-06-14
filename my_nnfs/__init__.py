5c406973ccc3d170fbb6f844f3d8d259cdb9a0e7
prafullofcode <dahal.praphul268@gmail.com>
2026-06-14 18:30:08 +0545

    feat:adaptive momentum (Adam) optimizer

--- my_nnfs/__init__.py
+++ my_nnfs/__init__.py
@@ -1,4 +1,4 @@
 from .layers import Layer_Dense
 from .activations import Activation_ReLU , Activation_Softmax
 from .losses import Activation_Softmax_Loss_CategoricalCrossentropy
-from .optimizers import Optimizer_SGD , Optimizer_AdaGrad , Optimizer_RMSProp
+from .optimizers import Optimizer_SGD , Optimizer_AdaGrad , Optimizer_RMSProp , Optimizer_Adam
