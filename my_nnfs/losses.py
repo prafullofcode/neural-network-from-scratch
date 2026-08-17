f5aa01eaefad208b20787f402c74846d70c0dda5
prafullofcode <dahal.praphul268@gmail.com>
2026-08-17 10:12:40 +0545

    feat:implement l1 and l2 regularization

--- my_nnfs/losses.py
+++ my_nnfs/losses.py
@@ -9,6 +9,23 @@ class Loss :
 
         return data_loss
 
+    def regularization_loss(self, layer):
+        regularization_loss = 0
+
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(layer.weights * layer.weights)
+
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(layer.biases * layer.biases)
+
+        return regularization_loss
+
 
 class Loss_CategoricalCrossEntropy(Loss):
     
@@ -41,7 +58,7 @@ class Loss_CategoricalCrossEntropy(Loss):
 
         self.dinputs = self.dinputs / samples
 
-class Activation_Softmax_Loss_CategoricalCrossentropy():
+class Activation_Softmax_Loss_CategoricalCrossentropy(Loss):
 
 
     def __init__(self):
